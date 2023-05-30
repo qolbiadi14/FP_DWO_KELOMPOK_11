eb5340b94ba7937c3b4227fc81a7cce21c206d5a
qolbiadi14 <nzqadii14@gmail.com>
2023-05-30 19:52:31 +0700

    menambah file untuk php

--- vendor.php
+++ vendor.php
@@ -6,17 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Dashboard</title>
 
-    <?php include "header.php"?>
+    <?php include "header.php" ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
-
-        <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div>
-
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <!-- Left navbar links -->
@@ -201,7 +195,7 @@
     </div>
     <!-- ./wrapper -->
 
-    <?php include "pluginScript.php"?>
+    <?php include "pluginScript.php" ?>
 </body>
 
 </html>
